fac8f84b26f6aeac153146b15189673a2f3bbd18
krcoombes <krcoombes@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-08-01 00:29:18 +0000

    Modified test of betaamix to avoid R CMD check failures on obsolete 32-bit machines.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/newmanomics@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/NewmanOmics/tests/03-betamix.R
+++ pkg/NewmanOmics/tests/03-betamix.R
@@ -8,8 +8,11 @@ set.seed(12345)
 normal <- lung[, 1, drop=FALSE]
 tumor  <- lung[, 2, drop=FALSE]
 ps <- pairedStat(normal, tumor)
+summary(ps@p.values)
 
-fm <- fitMix3(as.vector(ps@p.values), print.level = 2)
+fm <- fitMix3(as.vector(ps@p.values), print.level = 0)
+fm@mle
+round(fm@psi, 5)
 
 plot(fm)
 
